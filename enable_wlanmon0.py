0657f4db1fde2beb1b35a894ba1594d582d8e9ce
Mike <b00066331@student.itb.ie>
2016-02-27 23:38:34 +0000

    Commit

---
+++ enable_wlanmon0.py
@@ -0,0 +1,337 @@
+'''
+Created on Feb 13, 2016
+
+@author: mihhail
+'''
+#!/usr/bin/env python
+
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR) # Shut up Scapy
+
+from scapy.all import *
+conf.verb = 0 # Scapy I thought I told you to shut up
+import os
+import sys
+import time
+from threading import Thread, Lock
+from subprocess import Popen, PIPE
+from signal import SIGINT, signal
+import argparse
+import socket
+import struct
+import fcntl
+
+# Console colors
+W  = '\033[0m'  # white (normal)
+R  = '\033[31m' # red
+G  = '\033[32m' # green
+O  = '\033[33m' # orange
+B  = '\033[34m' # blue
+P  = '\033[35m' # purple
+C  = '\033[36m' # cyan
+GR = '\033[37m' # gray
+T  = '\033[93m' # tan
+
+def parse_args():
+    #Create the arguments
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i", "--interface", help="Choose monitor mode interface. By default script will find the most powerful interface and starts monitor mode on it. Example: -i mon5")
+    parser.add_argument("-c", "--channel", help="Listen clients on the specified channel. Example: -c 6")
+    parser.add_argument("-m", "--maximum", help="Choose the maximum number of SSIDs. List of SSIDs will be emptied and re-populated after hitting the limit. Example: -m 5")
+    
+    return parser.parse_args()
+
+
+########################################
+# Begin interface info and manipulation
+########################################
+
+def get_mon_iface(args):
+    global monitor_on
+    monitors, interfaces = iwconfig()
+    if args.interface:
+        monitor_on = True
+        return args.interface
+    if len(monitors) > 0:
+        monitor_on = True
+        return monitors[0]
+    else:
+        # Start monitor mode on a wireless interface
+        print '['+G+'*'+W+'] Finding the most powerful interface...'
+        interface = get_iface(interfaces)
+        monmode = start_mon_mode(interface)
+        return monmode
+
+def iwconfig():
+    monitors = []
+    interfaces = {}
+    try:
+        proc = Popen(['iwconfig'], stdout=PIPE, stderr=DN)
+    except OSError:
+        sys.exit('['+R+'-'+W+'] Could not execute "iwconfig"')
+    for line in proc.communicate()[0].split('\n'):
+        if len(line) == 0: continue # Isn't an empty string
+        if line[0] != ' ': # Doesn't start with space
+            wired_search = re.search('eth[0-9]|em[0-9]|p[1-9]p[1-9]', line)
+            if not wired_search: # Isn't wired
+                iface = line[:line.find(' ')] # is the interface
+                if 'Mode:Monitor' in line:
+                    monitors.append(iface)
+                elif 'IEEE 802.11' in line:
+                    if "ESSID:\"" in line:
+                        interfaces[iface] = 1
+                    else:
+                        interfaces[iface] = 0
+    return monitors, interfaces
+
+def get_iface(interfaces):
+    scanned_aps = []
+
+    if len(interfaces) < 1:
+        sys.exit('['+R+'-'+W+'] No wireless interfaces found, bring one up and try again')
+    if len(interfaces) == 1:
+        for interface in interfaces:
+            return interface
+
+    # Find most powerful interface
+    for iface in interfaces:
+        count = 0
+        proc = Popen(['iwlist', iface, 'scan'], stdout=PIPE, stderr=DN)
+        for line in proc.communicate()[0].split('\n'):
+            if ' - Address:' in line: # first line in iwlist scan for a new AP
+               count += 1
+        scanned_aps.append((count, iface))
+        print '['+G+'+'+W+'] Networks discovered by '+G+iface+W+': '+T+str(count)+W
+    try:
+        interface = max(scanned_aps)[1]
+        return interface
+    except Exception as e:
+        for iface in interfaces:
+            interface = iface
+            print '['+R+'-'+W+'] Minor error:',e
+            print '    Starting monitor mode on '+G+interface+W
+            return interface
+
+def start_mon_mode(interface):
+    print '['+G+'+'+W+'] Starting monitor mode off '+G+interface+W
+    try:
+        os.system('ifconfig %s down' % interface)
+        os.system('iwconfig %s mode monitor' % interface)
+        os.system('ifconfig %s up' % interface)
+        return interface
+    except Exception:
+        sys.exit('['+R+'-'+W+'] Could not start monitor mode')
+
+def remove_mon_iface(mon_iface):
+    os.system('ifconfig %s down' % mon_iface)
+    os.system('iwconfig %s mode managed' % mon_iface)
+    os.system('ifconfig %s up' % mon_iface)
+
+def mon_mac(mon_iface):
+    '''
+    http://stackoverflow.com/questions/159137/getting-mac-address
+    '''
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927, struct.pack('256s', mon_iface[:15]))
+    mac = ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
+    print '['+G+'*'+W+'] Monitor mode: '+G+mon_iface+W+' - '+O+mac+W
+    return mac
+
+########################################
+# End of interface info and manipulation
+########################################
+
+
+def channel_hop(mon_iface, args):
+    '''
+    First time it runs through the channels it stays on each channel for 5 seconds
+    in order to populate the AP list nicely. After that it goes as fast as it can
+    '''
+    global monchannel, first_pass
+
+    channelNum = 0
+    maxChan = 13 
+    err = None
+
+    while 1:
+        if args.channel:
+            with lock:
+                monchannel = args.channel
+        else:
+            channelNum +=1
+            if channelNum > maxChan:
+                channelNum = 1
+                with lock:
+                    first_pass = 0
+            with lock:
+                monchannel = str(channelNum)
+
+            try:
+                proc = Popen(['iw', 'dev', mon_iface, 'set', 'channel', monchannel], stdout=DN, stderr=PIPE)
+            except OSError:
+                print '['+R+'-'+W+'] Could not execute "iw"'
+                os.kill(os.getpid(),SIGINT)
+                sys.exit(1)
+            for line in proc.communicate()[1].split('\n'):
+                if len(line) > 2: # iw dev shouldnt display output unless there's an error
+                    err = '['+R+'-'+W+'] Channel hopping failed: '+R+line+W
+
+        output(err, monchannel)
+        
+def output(err, monchannel):
+    os.system('clear')
+    if err:
+        print err
+    else:
+        print '['+G+'+'+W+'] '+mon_iface+' channel: '+G+monchannel+W+'\n'
+    if len(APs) > 0:
+        print '\n      Access Points     ch   ESSID'
+    with lock:
+        for ap in APs:
+            print '['+T+'*'+W+'] '+O+ap[0]+W+' - '+ap[1].ljust(2)+' - '+T+ap[2]+W
+    print ''
+
+def noise_filter(skip, addr1, addr2):
+    # Broadcast, broadcast, IPv6mcast, spanning tree, spanning tree, multicast, broadcast
+    ignore = ['ff:ff:ff:ff:ff:ff', '00:00:00:00:00:00', '33:33:00:', '33:33:ff:', '01:80:c2:00:00:00', '01:00:5e:', mon_MAC]
+    if skip:
+        ignore.append(skip)
+    for i in ignore:
+        if i in addr1 or i in addr2:
+            return True
+
+def cb(pkt):
+    '''
+    Look for dot11 packets that aren't to or from broadcast address,
+    are type 1 or 2 (control, data), and append the addr1 and addr2
+    to the list of deauth targets.
+    '''
+    global clients_APs, APs
+
+    # return these if's keeping clients_APs the same or just reset clients_APs?
+    # I like the idea of the tool repopulating the variable more
+    if args.maximum:
+        if args.noupdate:
+            if len(clients_APs) > int(args.maximum):
+                return
+        else:
+            if len(clients_APs) > int(args.maximum):
+                with lock:
+                    clients_APs = []
+                    APs = []
+
+    # We're adding the AP and channel to the AP list at time of creation rather
+    # than updating on the fly in order to avoid costly for loops that require a lock
+    if pkt.haslayer(Dot11):
+        if pkt.addr1 and pkt.addr2:
+            pkt.addr1 = pkt.addr1.lower()
+            pkt.addr2 = pkt.addr2.lower()
+
+            # Filter out all other APs and clients if asked
+            if args.accesspoint:
+                if args.accesspoint not in [pkt.addr1, pkt.addr2]:
+                    return
+
+            # Check if it's added to our AP list
+            if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
+                APs_add(clients_APs, APs, pkt, args.channel, args.world)
+
+            # Ignore all the noisy packets like spanning tree
+
+            if noise_filter(skip, pkt.addr1, pkt.addr2):
+                return
+
+            # Management = 1, data = 2
+            if pkt.type in [1, 2]:
+                clients_APs_add(clients_APs, pkt.addr1, pkt.addr2)
+
+def APs_add(clients_APs, APs, pkt, chan_arg, world_arg):
+    ssid       = pkt[Dot11Elt].info
+    bssid      = pkt[Dot11].addr3.lower()
+    try:
+        # Thanks to airoscapy for below
+        ap_channel = str(ord(pkt[Dot11Elt:3].info))
+        chans = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'] if not args.world else ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'] 
+        if ap_channel not in chans:
+            return
+
+        if chan_arg:
+            if ap_channel != chan_arg:
+                return
+
+    except Exception as e:
+        return
+
+    if len(APs) == 0:
+        with lock:
+            return APs.append([bssid, ap_channel, ssid])
+    else:
+        for b in APs:
+            if bssid in b[0]:
+                return
+        with lock:
+            return APs.append([bssid, ap_channel, ssid])
+
+def clients_APs_add(clients_APs, addr1, addr2):
+    if len(clients_APs) == 0:
+        if len(APs) == 0:
+            with lock:
+                return clients_APs.append([addr1, addr2, monchannel])
+        else:
+            AP_check(addr1, addr2)
+
+    # Append new clients/APs if they're not in the list
+    else:
+        for ca in clients_APs:
+            if addr1 in ca and addr2 in ca:
+                return
+
+        if len(APs) > 0:
+            return AP_check(addr1, addr2)
+        else:
+            with lock:
+                return clients_APs.append([addr1, addr2, monchannel])
+
+def AP_check(addr1, addr2):
+    for ap in APs:
+        if ap[0].lower() in addr1.lower() or ap[0].lower() in addr2.lower():
+            with lock:
+                return clients_APs.append([addr1, addr2, ap[1], ap[2]])
+
+def stop(signal, frame):
+    if monitor_on:
+        sys.exit('\n['+R+'!'+W+'] Closing')
+    else:
+        remove_mon_iface(mon_iface)
+        os.system('service network-manager restart')
+        sys.exit('\n['+R+'!'+W+'] Closing')
+
+if __name__ == "__main__":
+    if os.geteuid():
+        sys.exit('['+R+'-'+W+'] Please run as root')
+    clients_APs = []
+    APs = []
+    DN = open(os.devnull, 'w')
+    lock = Lock()
+    args = parse_args()
+    monitor_on = None
+    mon_iface = get_mon_iface(args)
+    conf.iface = mon_iface
+    mon_MAC = mon_mac(mon_iface)
+    first_pass = 1
+
+    # Start channel hopping
+    hop = Thread(target=channel_hop, args=(mon_iface, args))
+    hop.daemon = True
+    hop.start()
+
+    signal(SIGINT, stop)
+
+    try:
+       sniff(iface=mon_iface, store=0, prn=cb)
+    except Exception as msg:
+        remove_mon_iface(mon_iface)
+        os.system('service network-manager restart')
+        print '\n['+R+'!'+W+'] Closing'
+        sys.exit(0)
---
+++ manuf.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+
+# manuf.py: Parser library for Wireshark's OUI database.
+# Copyright (c) 2016 Michael Huang
+#
+# This library is free software. It is dual licensed under the
+# terms of the GNU Lesser General Public License version 3.0
+# (or any later version) and the Apache License version 2.0.
+#
+# For more information, see:
+#
+# <http://www.gnu.org/licenses/>
+# <http://www.apache.org/licenses/>
+
+import re
+import sys
+
+from collections      import namedtuple
+try:
+    from cStringIO    import StringIO
+except ImportError:
+    try:
+        from StringIO import StringIO
+    except ImportError:
+        from io       import StringIO
+
+# Vendor tuple
+vendor = namedtuple('Vendor', ['manuf', 'comment'])
+
+class MacParser(object):
+    def  __init__(self, manuf_name = "manuf"):
+        """Class that contains a parser for Wireshark's OUI database.
+
+        Optimized for quick lookup performance by reading the entire file into
+        memory on initialization. Maps ranges of MAC addresses to manufacturers and
+        comments (descriptions). Contains full support for netmasks and other
+        strange things in the database.
+
+        See https://www.wireshark.org/tools/oui-lookup.html
+
+        Args:
+            manuf_name (str): Location of the manuf database file. Defaults to
+            "manuf" in the same directory.
+
+        Raises:
+            IOError: If manuf file could not be found.
+
+        """
+        self._manuf_name = manuf_name
+        self.refresh()
+
+    def refresh(self, manuf_name = None):
+        """Refresh/reload manuf database. Call this if database has been updated.
+
+        Args:
+            manuf_name (str): Location of the manuf data base file. Defaults to
+            "manuf" in the same directory.
+
+        Raises:
+            IOError: If manuf file could not be found.
+
+        """
+        if not manuf_name:
+            manuf_name = self._manuf_name
+        with open(manuf_name, 'r') as f:
+            manuf_file = StringIO(f.read())
+        self._masks = {}
+
+        # Build mask -> result dict
+        for line in manuf_file:
+            com = line.split('#', 1)
+            arr = com[0].split()
+
+            if len(arr) < 1:
+                continue
+
+            parts = arr[0].split('/')
+            mac_str = self._strip_mac(parts[0])
+            mac_int = self._get_mac_int(mac_str)
+            mask = self._bits_left(mac_str)
+
+            # Specification includes mask
+            if len(parts) > 1:
+                mask_spec = 48 - int(parts[1])
+                if mask_spec > mask:
+                    mask = mask_spec
+
+            if len(com) > 1:
+                result = vendor(manuf = arr[1], comment = com[1].strip())
+            else:
+                result = vendor(manuf = arr[1], comment = None)
+
+            self._masks[(mask,  mac_int >> mask)] = result
+
+        manuf_file.close()
+
+    def search(self, mac, max = 1):
+        """Search for multiple vendor tuples possibly matching a MAC address.
+
+        Args:
+            mac (str): MAC address in standard format.
+            max (Optional[int]): Maximum results to return. Defaults to 1.
+
+        Returns:
+            List of vendor namedtuples containing (manuf, comment), with closest
+            result first. May be empty if no results found.
+
+        Raises:
+            ValueError: If the MAC could not be parsed.
+
+        """
+        vendors = []
+        if max <= 0:
+            return vendors
+        mac_str = self._strip_mac(mac)
+        mac_int = self._get_mac_int(mac_str)
+
+        # If the user only gave us X bits, check X bits. No partial matching!
+        for mask in range(self._bits_left(mac_str), 48):
+            result = self._masks.get((mask, mac_int >> mask))
+            if result:
+                vendors.append(result)
+                if len(vendors) >= max:
+                    break
+        return vendors
+
+    def get_all(self, mac):
+        """Get a vendor tuple containing (manuf, comment) from a MAC address.
+
+        Args:
+            mac (str): MAC address in standard format.
+
+        Returns:
+            vendor: Vendor namedtuple containing (manuf, comment). Either or
+            both may be None if not found.
+
+        Raises:
+            ValueError: If the MAC could not be parsed.
+
+        """
+        vendors = self.search(mac)
+        if len(vendors) == 0:
+            return vendor(manuf = None, comment = None)
+        return vendors[0]
+
+    def get_manuf(self, mac):
+        """Returns manufacturer from a MAC address.
+
+        Args:
+            mac (str): MAC address in standard format.
+
+        Returns:
+            string: String containing manufacturer, or None if not found.
+
+        Raises:
+            ValueError: If the MAC could not be parsed.
+
+        """
+        return self.get_all(mac).manuf
+
+    def get_comment(self, mac):
+        """Returns comment from a MAC address.
+
+        Args:
+            mac (str): MAC address in standard format.
+
+        Returns:
+            string: String containing comment, or None if not found.
+
+        Raises:
+            ValueError: If the MAC could not be parsed.
+
+        """
+        return self.get_all(mac).comment
+
+    # Gets the integer representation of a stripped mac string
+    def _get_mac_int(self, mac_str):
+        try:
+            # Fill in missing bits with zeroes
+            return int(mac_str, 16) << self._bits_left(mac_str)
+        except ValueError:
+            raise ValueError("Could not parse MAC: "+str(mac_str))
+
+    # Strips the MAC address of '-', ':', and '.' characters
+    _strip_mac = lambda self, mac: self._pattern.sub("", mac)
+    _pattern = re.compile("[-:\.]")
+
+    # Gets the number of bits left in a mac string
+    _bits_left = lambda self, mac_str: 48 - 4 * len(mac_str)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("manuf.py: Parser utility for Wireshark's OUI database.")
+        print("    Usage: manuf.py <mac-address> [<manuf-file-path>]")
+        sys.exit(0)
+    elif len(sys.argv) > 2:
+        parser = MacParser(sys.argv[2])
+    else:
+        parser = MacParser()
+    print(parser.get_all(sys.argv[1]))
+    sys.exit(0)
