adb4739d6ba23c169b458fb6bcdaffee6c31871a
Mike <Mihhail_Shapovalov@L-089551-W8.SYMC.SYMANTEC.COM>
2016-03-24 16:39:07 +0000

    updated db create script

--- probecapsql.py
+++ probecapsql.py
@@ -18,6 +18,19 @@ try:
     cur.execute("""CREATE USER probecap WITH PASSWORD 'pass';""")
     cur.execute("""ALTER USER probecap WITH LOGIN;""")
     cur.execute("""ALTER USER probecap WITH SUPERUSER;""")
+    cur.close
+    con.close
+except psycopg2.DatabaseError, e:
+    print 'Error %s' % e    
+
+con = None
+con = psycopg2.connect(database='wifi', user='probecap', host = 'localhost', password='pass')
+print "Connecting to database\n    ->%s" % (con)
+
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+cur = con.cursor()
+
+try:
     cur.execute('CREATE TABLE IF NOT EXISTS station(id serial not null UNIQUE,mac macaddr not null,firstSeen timestamp without time zone not null,lastSeen timestamp without time zone,PRIMARY KEY(mac));')
     cur.execute('grant usage,select on sequence station_id_seq to probecap;')
     cur.execute('CREATE TABLE IF NOT EXISTS ssid(id serial not null UNIQUE,name varchar(32) not null,PRIMARY KEY(name));')
